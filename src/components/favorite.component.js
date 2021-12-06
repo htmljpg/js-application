b5fa93bfd3ba3eaa0034e1caf06721e65b253aa5
htmljpg <htm.jpg@mail.ru>
2021-12-06 21:19:55 +0400

    Js application

---
+++ src/components/favorite.component.js
@@ -0,0 +1,50 @@
+import { Component } from "../core/component";
+import { apiService } from "../services/api.service";
+import { renderPosts } from "../template/post.template";
+
+export class FavoriteComponent extends Component {
+    constructor(id, options) {
+        super(id);
+        
+        this.loader = options.loader;
+    }
+
+    init() {
+        this.$el.addEventListener('click', linkClickHandler.bind(this));
+    }
+
+    onShow() {
+        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        const html = renderList(favorites);
+        this.$el.insertAdjacentHTML('afterbegin', html);
+    }
+
+    onHide() {
+        this.$el.innerHTML = '';
+    }
+}
+
+async function linkClickHandler(event) {
+    event.preventDefault();
+    
+    if (event.target.classList.contains('js-link')) {
+        const id = event.target.dataset.id;
+        this.$el.innerHTML = '';
+        this.loader.show();
+        const post = await apiService.fetchPostById(id);
+        this.loader.hide();
+        this.$el.insertAdjacentHTML('afterbegin', renderPosts(post, {withButton: false}));
+    }
+}
+
+function renderList(list) {
+    if (list.length) {
+        return `
+            <ul>
+                ${list.map(i => `<li><a href="#" class="js-link" data-id="${i.id}">${i.title}</a></li>`).join(' ')}
+            </ul>
+        `;
+    }
+
+    return '<p>Ничего не найдено</p>';
+}
---
+++ src/components/posts.component.js
@@ -0,0 +1,55 @@
+import { Component } from "../core/component";
+import { apiService } from "../services/api.service";
+import { TransformService } from "../services/transform.service";
+import { renderPosts } from "../template/post.template";
+
+export class PostsComponent extends Component {
+    constructor(id, {loader}) {
+        super(id);
+        this.loader = loader;
+    }
+
+    init() {
+        this.$el.addEventListener('click', buttonHandler.bind(this));
+    }
+
+    async onShow() {
+        this.loader.show();
+        const fbData = await apiService.fetchPosts();
+        const posts = TransformService.fbObjectToArray(fbData);
+        const html = posts.map(post => renderPosts(post, {withButton: true}));
+        this.loader.hide();
+        this.$el.insertAdjacentHTML('afterbegin', html.join(' '));
+    }
+
+    onHide() {
+        this.$el.innerHTML = '';
+    }
+}
+
+function buttonHandler(event) {
+    const $el = event.target;
+    const id = event.target.dataset.id;
+    const title = event.target.dataset.title;
+
+    if (id) {
+        let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        const candidate = favorites.find(f => f.id === id);
+
+        if (candidate) {
+            // удалить элемент
+            favorites = favorites.filter(f => f.id !== id);
+            $el.textContent = 'Сохранить';
+            $el.classList.remove('button-danger');
+            $el.classList.add('button-primary');
+        } else {
+            // добавить элемент
+            favorites.push({id, title});
+            $el.textContent = 'Удалить';
+            $el.classList.add('button-danger');
+            $el.classList.remove('button-primary');
+        }
+
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+}
